1bd96387fa8e12c55fd710a746960087b45bade8
ta4anka <vladymirsheyko@gmail.com>
2025-04-21 03:26:05 +0200

    init favorites for practice-4

---
+++ practice-4/home_work/favorites_v2/programs/favorites_v2/src/lib.rs
@@ -0,0 +1,50 @@
+use anchor_lang::prelude::*;
+
+declare_id!("646xu1j5tJ56w1hiPmdjHo2KV4hXg4kCC7Xr3aRmDU4V");
+
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+    #[max_len(50)]
+    pub color: String,
+}
+
+#[derive(Accounts)]
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
+        seeds = [b"favorites", user.key().as_ref()],
+        bump,
+    )]
+    pub favorites: Account<'info, Favorites>,
+    pub system_program: Program<'info, System>,
+}
+
+#[program]
+pub mod favorites {
+    use super::*;
+    pub fn set_favorites(context: Context<SetFavorites>, number: u64, color: String) -> Result<()> {
+        let user_public_key = context.accounts.user.key();
+        msg!("Greetings from {}", context.program_id);
+        msg!(
+            "User {}'s favorite number is {} and favorite color is: {}",
+            user_public_key,
+            number,
+            color
+        );
+
+        context
+            .accounts
+            .favorites
+            .set_inner(Favorites { number, color });
+        Ok(())
+    }
+}
